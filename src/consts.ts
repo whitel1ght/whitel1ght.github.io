a4468580fed408744b2b283df242e09be3bf9813
Dmitry Mamyrev <dmitry.mamyrev@epicmax.co>
2025-01-02 20:51:44 +0300

    about and index pages fixes

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Dmitry Mamyrev\'s Blog';
+export const SITE_DESCRIPTION = 'Welcome to my Web Developer Blog – your ultimate destination for the latest trends, tutorials, and insights in web development! Whether you\'re a beginner looking to learn the basics of HTML, CSS, and JavaScript, or an experienced developer aiming to master advanced frameworks like React, Angular, and Vue.js, I\'ve got you covered. Stay updated with cutting-edge technologies, best practices in coding, and expert tips to enhance your development skills.';
