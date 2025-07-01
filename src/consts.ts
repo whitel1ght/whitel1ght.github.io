1319ca212b116bcaba3d652548391e880b6bce9d
Dmitry Mamyrev <dmitry.mamyrev@epicmax.co>
2025-07-01 16:07:43 +0300

    fix description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Dmitry Mamyrev\'s Blog';
-export const SITE_DESCRIPTION = 'Welcome to my Web Developer Blog – your ultimate destination for the latest trends, tutorials, and insights in web development! Whether you\'re a beginner looking to learn the basics of HTML, CSS, and JavaScript, or an experienced developer aiming to master advanced frameworks like React, Angular, and Vue.js, I\'ve got you covered. Stay updated with cutting-edge technologies, best practices in coding, and expert tips to enhance your development skills.';
+export const SITE_DESCRIPTION = 'A personal blog by Dmitry Mamyrev sharing human perspectives on coding, open source, productivity hacks, and blah blah blah';
